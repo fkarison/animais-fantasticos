6cf2266683d0393dc0c4809a91de57833d9a47e9
Felipe Karison da Silva <fkarison@uolinc.com>
2022-07-01 16:56:23 -0300

    TabNav transformado em Classe

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,22 +1,33 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll("[data-tab='menu'] li");
-  const tabContent = document.querySelectorAll("[data-tab='content'] section");
-
-  // Adicionou a classe ativo a primeira section
-  tabContent[0].classList.add("ativo");
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo"); // Remove a classe ativo de todas as sections
-    });
-    const direcao = tabContent[index].dataset.anime; // retorna o nome do atributo data-anime
-    tabContent[index].classList.add("ativo", direcao); // Adiciona uma classe 'ativo' ao index selecionado
+export default class initTabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  // ForEach para a cada click nas LIs de tabMenu ele ativa a função activeTab e recebe o index do itemMenu
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
+  // Ativa a tab de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass); // Remove a classe ativo de todas as sections
     });
-  });
+    const direcao = this.tabContent[index].dataset.anime; // retorna o nome do atributo data-anime
+    this.tabContent[index].classList.add(this.activeClass, direcao); // Adiciona uma classe 'ativo' ao index selecionado
+  }
+
+  // Adiciona os eventos nas tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        this.activeTab(index);
+      });
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // Ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Scrollsuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
-import initTabNav from './modules/tab-nav.js';
+import TabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
 import initDropDown from './modules/dropdown.js';
@@ -16,7 +16,12 @@ scrollSuave.init();
 const accordion = new Accordion("[data-anime='accordion'] dt");
 accordion.init();
 
-initTabNav();
+const tabNav = new TabNav(
+  "[data-tab='menu'] li",
+  "[data-tab='content'] section"
+);
+tabNav.init();
+
 initModal();
 initToolTip();
 initDropDown();

dec22fcfda5e6780b4be39a43ad42b9e76289854
Felipe Karison da Silva <fkarison@uolinc.com>
2022-07-01 15:10:11 -0300

    ScrollSuave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import initScrollSuave from './modules/scroll-suave.js';
+import Scrollsuave from './modules/scroll-suave.js';
 import initScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
@@ -10,7 +10,9 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
-initScrollSuave();
+const scrollSuave = new Scrollsuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 initScroll();
 initAccordion();
 initTabNav();

702453f6b3f4e468f4cbdb04ff0aac64af910a05
Felipe Karison da Silva <fkarison@uolinc.com>
2022-07-06 10:43:24 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,21 +1,43 @@
-export default function initModal() {
-  const abrir = document.querySelector('[data-modal="abrir"]');
-  const fechar = document.querySelector('[data-modal="fechar"]');
-  const modal = document.querySelector('[data-modal="container"]');
-  function toggleModal(event) {
-    event.preventDefault();
-    modal.classList.toggle('ativo');
+export default class Modal {
+  constructor(abrir, fechar, containerModal) {
+    this.abrir = document.querySelector(abrir);
+    this.fechar = document.querySelector(fechar);
+    this.containerModal = document.querySelector(containerModal);
+    // bind this ao callback para fazer referência ao objeto da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clicarForaModal = this.clicarForaModal.bind(this);
   }
 
-  function clicarForaModal(event) {
-    if (event.target === this) {
-      toggleModal(event);
+  // Abre ou fecha o modal
+  toggleModal() {
+    this.containerModal.classList.toggle('ativo');
+  }
+
+  // Adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  // Fecha o modal ao clicar fora
+  clicarForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal();
     }
   }
-  // Verifica se esses elementos existem, caso sim, executa a função, caso contrário, não retorna erro.
-  if (abrir && fechar && modal) {
-    abrir.addEventListener('click', toggleModal);
-    fechar.addEventListener('click', toggleModal);
-    modal.addEventListener('click', clicarForaModal);
+
+  // Adiciona os eventos aos elementos do modal
+  addModalEvent() {
+    this.abrir.addEventListener('click', this.eventToggleModal);
+    this.fechar.addEventListener('click', this.eventToggleModal);
+    this.containerModal.addEventListener('click', this.clicarForaModal);
+  }
+
+  init() {
+    // Verifica se esses elementos existem, caso sim, executa a função, caso contrário, não retorna erro.
+    if (this.abrir && this.fechar && this.containerModal) {
+      this.addModalEvent();
+    }
+    return this;
   }
 }

b8fc026a7f4e520f3ed8c9b71c43f9d96f001824
Felipe Karison da Silva <fkarison@uolinc.com>
2022-07-06 18:46:54 -0300

    anima-scroll refeito

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,25 +1,43 @@
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections); // retorna as sections com a classe .js-scroll
-    this.windowsCorte = window.innerHeight * 0.65; // define que o topo da página será 65% do tamanho da tela
-    this.animaScroll = this.animaScroll.bind(this);
+    this.windowsCorte = window.innerHeight * 0.6; // define que o topo da página será 65% do tamanho da tela
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top; // retorna a altura que cada section está em relação ao topo
-      const isSectionVisible = sectionTop - this.windowsCorte < 0; // se a altura da section em relação ao topo - 65% da tela for menor que 0 retorne verdadeiro
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  // Pega a distância de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop; // retorna a altura que cada section está em relação ao topo
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowsCorte),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
   }
-  // IF abaixo verifica se sections existe para executar o restante do código
+
+  // Remove o event de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
+  }
 }
